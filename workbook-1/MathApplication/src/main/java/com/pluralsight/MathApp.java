67152f01ef1954b3c00dc702ec2ea97d969712cd
tonsorialdev <147235191+tonsorialdev@users.noreply.github.com>
2024-04-11 15:47:43 -0700

    April 11 2024

--- workbook-1/MathApplication/src/main/java/com/pluralsight/MathApp.java
+++ workbook-1/MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -1,7 +1,13 @@
 package com.pluralsight;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MathApp
 {
+
+
     public static void main(String[] args)
     {
         // find all 7 questions in your workbook on page 
@@ -20,12 +26,16 @@ public class MathApp
     {
         // Question 1:
         // declare variables here
+    int bobSalary = 10000;
+    int garySalary = 20000;
+
 
 
         // code the logic to the problem here
-
+        int highestSalary = Math.max(bobSalary , garySalary);
 
         // use System.out.println() to display results
+        System.out.println("The highest salary is " + highestSalary);
         // ex: System.out.println("The answer is " + answer);
     }
 
@@ -35,12 +45,15 @@ public class MathApp
     {
         // Question 2:
         // declare variables here
+ int carPrice = 20000;
+ int truckPrice = 45000;
 
 
         // code the logic to the problem here
-
+int vehicleWithSmallerPrice = Math.min(carPrice, truckPrice);
 
         // use System.out.println() to display results
+        System.out.println("Smallest price is " + vehicleWithSmallerPrice);
         // ex: System.out.println("The answer is " + answer);
     }
 
@@ -50,12 +63,14 @@ public class MathApp
     {
         // Question 3:
         // declare variables here
+double circleRadius = 7.25;
 
 
         // code the logic to the problem here
-
+double circleArea = Math.PI * Math.pow(circleRadius,2);
 
         // use System.out.println() to display results
+        System.out.println("Area is : " + circleArea);
         // ex: System.out.println("The answer is " + answer);
     }
 
@@ -65,12 +80,13 @@ public class MathApp
     {
         // Question 4:
         // declare variables here
-
+double varSquareRt = 5.0;
 
         // code the logic to the problem here
-
+double squareRtOfVar = Math.sqrt(varSquareRt);
 
         // use System.out.println() to display results
+        System.out.println(squareRtOfVar);
         // ex: System.out.println("The answer is " + answer);
     }
 
@@ -82,10 +98,25 @@ public class MathApp
         // declare variables here
 
 
+int pointX1 = 5;
+int pointY1 = 10;
+
+int pointX2 = 85;
+int pointY2 = 50;
+
+
+
         // code the logic to the problem here
 
 
+        double finalCordX = Math.pow((pointX2 - pointX1),2);
+        double finalCordY = Math.pow((pointY2-pointY1),2);
+        double finalDistance = Math.sqrt(finalCordX + finalCordY);
+
+
         // use System.out.println() to display results
+        System.out.println("Distance is " + finalDistance);
+
         // ex: System.out.println("The answer is " + answer);
 
     }
@@ -96,12 +127,14 @@ public class MathApp
     {
         // Question 6:
         // declare variables here
+double absVariable = -3.8;
 
 
         // code the logic to the problem here
-
+double absoluteValueOfVariable = Math.abs(absVariable);
 
         // use System.out.println() to display results
+        System.out.println(absoluteValueOfVariable);
         // ex: System.out.println("The answer is " + answer);
     }
 
@@ -111,12 +144,35 @@ public class MathApp
     {
         // Question 7:
         // declare variables here
-
+        double randomNum = Math.random();
 
         // code the logic to the problem here
 
 
         // use System.out.println() to display results
+        System.out.println(randomNum);
         // ex: System.out.println("The answer is " + answer);
+
+
+
+
+
+
+        // random number between 50 and 100
+
+        int j = 0;
+
+        List <Integer> ranges = new ArrayList<>();
+        for(int i = 50 ; i <= 100; i++){
+            ranges.add(i);
+        }
+
+
+        for(int i = 50; i <= ranges.size() ; i++){
+           j = (int)(Math.random() * 100);
+            i = j;
+        }
+        System.out.println(j);
+
     }
 }
--- workbook-1/demos/03-IntroToJava/src/main/java/com/pluralsight/Main.java
+++ workbook-1/demos/03-IntroToJava/src/main/java/com/pluralsight/Main.java
@@ -2,8 +2,20 @@ package com.pluralsight;
 
 public class Main
 {
+    public class Car {
+        int vin;
+        String vehicleMake;
+        String vehicleColor;
+        boolean hasTowingPackage;
+        int odometerReading;
+        double price;
+        char qualityRating;
+        String phoneNumber;
+        String ssn;
+        int zipCode;
+    }
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        System.out.println("Hello Gregor");
     }
 }
