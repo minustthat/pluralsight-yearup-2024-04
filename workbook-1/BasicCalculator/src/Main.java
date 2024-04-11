072cf7056b220706be40194c6cd3096a692fe9b2
tonsorialdev <147235191+tonsorialdev@users.noreply.github.com>
2024-04-11 16:35:41 -0700

    Scanner Ex

---
+++ workbook-1/BasicCalculator/src/Main.java
@@ -0,0 +1,39 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input two numbers: ");
+        float numberA = scanner.nextFloat();
+        float numberB = scanner.nextFloat();
+        System.out.printf("What would you like to do with these numbers? Press \n (A) for add \n (S) for subtract \n (M) for Multiply \n (D) for divide \n ");
+        String userChoice = scanner.next();
+        float result;
+        switch (userChoice){
+            case "A":
+                result = numberA + numberB;
+
+            case "S":
+                result = numberA - numberB;
+
+            case "M":
+                result = numberA * numberB;
+
+            case "D":
+                result = numberA / numberB;
+            default:
+                result = numberA + numberB;
+
+        }
+        System.out.println(result);
+
+
+
+
+
+
+    }
+}
